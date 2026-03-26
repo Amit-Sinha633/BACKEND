714a16e767dc0a61ef9f7c6816d8a70e7584e5ec
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-26 17:39:55 +0530

    change

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: [  "*",
-      "https://desunlearnanderan.netlify.app"
-    ],
+    origin: "https://desunlearnanderan.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
