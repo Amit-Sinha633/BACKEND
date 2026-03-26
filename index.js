d509b03a98b3dd099f86b8b41a676da7fd9bb4ee
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-26 16:27:00 +0530

    change

--- index.js
+++ index.js
@@ -14,19 +14,23 @@ app.use(cookieParser());
 
 app.use(cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://desunlearnanderan.netlify.app"
     ],
 
     credentials: true,
      methods: ["GET", "POST", "PUT", "DELETE"]
-}))
+}));
 
 app.get('/', (req,res)=>{
   res.send("ohh Lovely")
 });
+
+
 app.use("/app/v1/Learn", router);
+
 const Port = process.env.PORT || 8000;
+
 connection();
 app.listen(Port, () => {
   console.log(`this is my localhost ${Port}`);
