735a5b7bd6ea88bcca2a38c5897e5ace67c9b532
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-26 16:10:38 +0530

    change

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ app.use(cors({
       "http://localhost:5173",
       "https://desunlearnanderan.netlify.app"
     ],
+
     credentials: true,
-     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 
 app.get('/', (req,res)=>{
