c61e3ec2319fda6689840c5503062a111ec4afba
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-26 16:15:45 +0530

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
       "http://localhost:5173",
-      "https://desunlearnanderan.netlify.app"
+      "https://desunlearnanderan.netlify.app/"
     ],
 
     credentials: true,
