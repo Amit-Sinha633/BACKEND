0c4b8d6d567bfa81165962489252f7b961aae426
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-26 13:29:41 +0530

    this is my controller

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connection from "./src/db/user.db.js";
-// import cors from "cors"
+import cors from "cors"
 import cookieParser from "cookie-parser";
 import router from "./src/routes/user.route.js";
 import dotenv from "dotenv";
@@ -10,10 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-// app.use(cors({
-//     origin: "https://desunlearnanderan.netlify.app/",
-//     credentials: true
-// }))
+app.use(cors({
+    origin: "https://desunlearnanderan.netlify.app/",
+    credentials: true
+}))
 
 app.get('/', (req,res)=>{
   res.send("ohh Lovely")
