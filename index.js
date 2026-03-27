dd193c7724f2f12c14476ad5a103b794b1a658b3
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-03-27 12:18:02 +0530

    change

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "https://frontend-desun-learn-and-eran.onrender.com",
+    origin: ["https://frontend-desun-learn-and-eran.onrender.com",
+      "http://loacalhost:5173"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
