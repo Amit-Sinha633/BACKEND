f352d1c6061f1aa8a15b49508cba1bd0d1b0a169
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-04-15 22:40:30 +0530

    something

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.use(cors({
       "http://localhost:5173"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"]
 }));
 
 
